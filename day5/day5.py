ffe0454a2920e11d7aecc03167d0d9173b843304
Bouke <b.grevelt@gmail.com>
2019-12-08 16:12:39 +0100

    run puzzle 1

--- day5/day5.py
+++ day5/day5.py
@@ -146,3 +146,10 @@ class IntcodeProcessorTests(unittest.TestCase):
         self.assertEqual(run([2, 3, 0, 3, 99]), [2, 3, 0, 6, 99])
         self.assertEqual(run([2, 4, 4, 5, 99, 0]), [2, 4, 4, 5, 99, 9801])
         self.assertEqual(run([1, 1, 1, 4, 99, 5, 6, 0, 99]), [30, 1, 1, 4, 2, 5, 6, 0, 99])
+
+def puzzle1():
+    with open('input.txt') as f:
+        memory = [int(n) for n in f.read().split(',')]
+        run(memory)
+
+puzzle1()
