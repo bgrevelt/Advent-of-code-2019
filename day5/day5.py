00bc346a1fcaad2fde60bf95632efc2448194b2d
Bouke <b.grevelt@gmail.com>
2019-12-08 17:10:14 +0100

    puzzle 2 Added jump and equality instructions

--- day5/day5.py
+++ day5/day5.py
@@ -31,23 +31,6 @@ class Instruction(abc.ABC):
         assert len(memory) > parameter, F"Can't access location {parameter} in memory with size {len(memory)}"
         memory[parameter] = value
 
-
-
-class Ternarynstruction(Instruction):
-    def __init__(self, operator, opcode):
-        super(Ternarynstruction, self).__init__(opcode, 3)
-        self._operator = operator
-
-    def process(self, memory, startat, parameter_modes):
-        self.get_input_parameters(memory, startat, parameter_modes)
-        super(Ternarynstruction, self).set_parameter(memory[startat+2], parameter_modes[2], memory, self._operator(self.parameter1, self.parameter2))
-
-        return startat + self.parameter_count()
-
-    def get_input_parameters(self, memory, startat, parameter_modes):
-        self.parameter1 = super(Ternarynstruction, self).get_parameter(memory[startat], parameter_modes[0], memory)
-        self.parameter2 = super(Ternarynstruction, self).get_parameter(memory[startat + 1], parameter_modes[1], memory)
-
 class NullaryInstruction(Instruction):
     def __init__(self, opcode):
         super(NullaryInstruction, self).__init__(opcode, 0)
@@ -56,13 +39,47 @@ class UnaryInstruction(Instruction):
     def __init__(self, opcode):
         super(UnaryInstruction, self).__init__(opcode, 1)
 
-class InstructionAdd(Ternarynstruction):
-    def __init__(self):
-        super(InstructionAdd, self).__init__(operator.add, 1)
+class BinaryInstruction(Instruction):
+    def __init__(self, opcode):
+        super(BinaryInstruction, self).__init__(opcode, 2)
 
-class InstructionMultiply(Ternarynstruction):
+    def get_parameters(self, memory, startat, parameter_modes):
+        self.parameter1 = super(BinaryInstruction, self).get_parameter(memory[startat], parameter_modes[0], memory)
+        self.parameter2 = super(BinaryInstruction, self).get_parameter(memory[startat + 1], parameter_modes[1], memory)
+
+class TernaryInstruction(Instruction):
+    def __init__(self, opcode):
+        super(TernaryInstruction, self).__init__(opcode, 3)
+
+    def store(self, value, memory):
+        assert len(memory) > self.parameter3, F"Can't access location {self.parameter3} in memory with size {len(memory)}"
+        memory[self.parameter3] = value
+
+    def get_parameters(self, memory, startat, parameter_modes):
+        self.parameter1 = super(TernaryInstruction, self).get_parameter(memory[startat], parameter_modes[0], memory)
+        self.parameter2 = super(TernaryInstruction, self).get_parameter(memory[startat + 1], parameter_modes[1], memory)
+        self.parameter3 = memory[startat + 2]
+
+
+
+class InstructionAdd(TernaryInstruction):
     def __init__(self):
-        super(InstructionMultiply, self).__init__(operator.mul, 2)
+        super(InstructionAdd, self).__init__(1)
+
+    def process(self, memory, startat, parameter_modes):
+        super(InstructionAdd, self).get_parameters(memory, startat, parameter_modes)
+        super(InstructionAdd, self).store(self.parameter1 + self.parameter2, memory)
+        return startat + self.parameter_count()
+
+
+class InstructionMultiply(TernaryInstruction):
+    def __init__(self):
+        super(InstructionMultiply, self).__init__(2)
+
+    def process(self, memory, startat, parameter_modes):
+        super(InstructionMultiply, self).get_parameters(memory, startat, parameter_modes)
+        super(InstructionMultiply, self).store(self.parameter1 * self.parameter2, memory)
+        return startat + self.parameter_count()
 
 class InstructionStore(UnaryInstruction):
     def __init__(self):
@@ -75,7 +92,6 @@ class InstructionStore(UnaryInstruction):
 
         return startat + self.parameter_count()
 
-
 class InstructionLoad(UnaryInstruction):
     def __init__(self):
         super(InstructionLoad, self).__init__(4)
@@ -85,6 +101,47 @@ class InstructionLoad(UnaryInstruction):
         print(param)
         return startat + self.parameter_count()
 
+class InstructionJumpIfTrue(BinaryInstruction):
+    def __init__(self):
+        super(InstructionJumpIfTrue, self).__init__(5)
+
+    def process(self, memory, startat, parameter_modes):
+        self.get_parameters(memory, startat, parameter_modes)
+        if(self.parameter1 != 0):
+            return self.parameter2
+        else:
+            return startat + self.parameter_count()
+
+class InstructionJumpIfFalse(BinaryInstruction):
+    def __init__(self):
+        super(InstructionJumpIfFalse, self).__init__(6)
+
+    def process(self, memory, startat, parameter_modes):
+        self.get_parameters(memory, startat, parameter_modes)
+        if(self.parameter1 == 0):
+            return self.parameter2
+        else:
+            return startat + self.parameter_count()
+
+class InstructionLessThen(TernaryInstruction):
+    def __init__(self):
+        super(InstructionLessThen, self).__init__(7)
+
+    def process(self, memory, startat, parameter_modes):
+        self.get_parameters(memory, startat, parameter_modes)
+        super(InstructionLessThen, self).store(1 if self.parameter1 < self.parameter2 else 0, memory)
+
+        return startat + self.parameter_count()
+
+class InstructionEquals(TernaryInstruction):
+    def __init__(self):
+        super(InstructionEquals, self).__init__(8)
+
+    def process(self, memory, startat, parameter_modes):
+        self.get_parameters(memory, startat, parameter_modes)
+        super(InstructionEquals, self).store(1 if self.parameter1 == self.parameter2 else 0, memory)
+
+        return startat + self.parameter_count()
 
 class InstructionHalt(NullaryInstruction):
     def __init__(self):
@@ -96,7 +153,7 @@ class InstructionHalt(NullaryInstruction):
 
 class IntcodeProcessor:
     def __init__(self):
-        self.operations = {operation.opcode() : operation for operation in [InstructionAdd(), InstructionMultiply(), InstructionHalt(), InstructionStore(), InstructionLoad()]}
+        self.operations = {operation.opcode() : operation for operation in [InstructionAdd(), InstructionMultiply(), InstructionHalt(), InstructionStore(), InstructionLoad(), InstructionJumpIfFalse(), InstructionJumpIfTrue(), InstructionLessThen(), InstructionEquals() ]}
 
     def Process(self, memory):
         instruction_pointer = 0
@@ -160,4 +217,4 @@ def puzzle1():
         memory = [int(n) for n in f.read().split(',')]
         run(memory)
 
-#puzzle1()
+puzzle1()
