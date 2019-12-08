05c8a2e1fb423b9e6ffd44fd255d5ade1076b5f8
Bouke <b.grevelt@gmail.com>
2019-12-08 15:52:36 +0100

    Added support for parameter mode and a bunch of unit tests

--- day5/day5.py
+++ day5/day5.py
@@ -3,28 +3,52 @@ import operator
 import unittest
 
 class Instruction(abc.ABC):
-    @abc.abstractmethod
-    def opcode(self):
-        pass
-
-    @abc.abstractmethod
-    def process(self, memory, startat):
-        pass
-
-class BinaryInstruction(Instruction):
-    def __init__(self, operator, opcode):
-        self._operator = operator
+    def __init__(self, opcode, parameter_count):
         self._opcode = opcode
-        self._number_of_parameters = 3
+        self._parameter_count = parameter_count
 
     def opcode(self):
         return self._opcode
 
-    def process(self, memory, startat):
-        parameters = memory[startat : startat + self._number_of_parameters]
+    @abc.abstractmethod
+    def process(self, memory, startat, parameter_modes):
+        pass
 
-        memory[parameters[2]] = self._operator(memory[parameters[0]],memory[parameters[1]])
-        return self._number_of_parameters
+    def parameter_count(self):
+        return self._parameter_count
+
+    def get_parameter(self, parameter, mode, memory):
+        if mode == 0:
+            assert len(memory) > parameter, F"Can't access location {parameter} in memory with size {len(memory)}"
+            return memory[parameter]
+        if mode == 1:
+            return parameter
+
+        assert False, F"Unsupported parameter mode {mode}"
+
+    def set_parameter(self, parameter, mode, memory, value):
+        assert mode == 0, F"Unsupported parameter mode for storing: {mode}"
+        assert len(memory) > parameter, F"Can't access location {parameter} in memory with size {len(memory)}"
+        memory[parameter] = value
+
+
+
+class BinaryInstruction(Instruction):
+    def __init__(self, operator, opcode):
+        super(BinaryInstruction, self).__init__(opcode, 3)
+        self._operator = operator
+
+    def process(self, memory, startat, parameter_modes):
+        parameter1 = super(BinaryInstruction, self).get_parameter(memory[startat], parameter_modes[0], memory)
+        parameter2 = super(BinaryInstruction, self).get_parameter(memory[startat+1], parameter_modes[1], memory)
+        super(BinaryInstruction, self).set_parameter(memory[startat+2], parameter_modes[2], memory, self._operator(parameter1, parameter2))
+
+class UnaryInstruction(Instruction):
+    def __init__(self, opcode):
+        super(UnaryInstruction, self).__init__(opcode, 0)
+
+    def process(self, memory, startat, parameter_modes):
+        return None
 
 
 class InstructionAdd(BinaryInstruction):
@@ -35,37 +59,68 @@ class InstructionMultiply(BinaryInstruction):
     def __init__(self):
         super(InstructionMultiply, self).__init__(operator.mul, 2)
 
+class InstructionHalt(UnaryInstruction):
+    def __init__(self):
+        super(InstructionHalt, self).__init__(99)
+
 
 class IntcodeProcessor:
     def __init__(self):
-        self.operations = {operation.opcode() : operation for operation in [InstructionAdd(), InstructionMultiply()]}
+        self.operations = {operation.opcode() : operation for operation in [InstructionAdd(), InstructionMultiply(), InstructionHalt()]}
 
     def Process(self, memory):
         instruction_pointer = 0
         while True:
-            opcode = memory[instruction_pointer]
+            opcode, parameter_modes = self.split_instruction(memory[instruction_pointer])
             if opcode == 99:
                 break
 
+
             instruction_pointer += 1
             assert opcode in self.operations, F"Unknown opcode {opcode}"
-            tokens_consumed = self.operations[opcode].process(memory, instruction_pointer)
-            instruction_pointer += tokens_consumed
+            self.operations[opcode].process(memory, instruction_pointer, parameter_modes)
+            instruction_pointer += self.operations[opcode].parameter_count()
 
+    def split_instruction(self, instruction):
+        opcode = instruction % 100
+        assert opcode in self.operations, F"Uknown opcode {opcode}"
+        parameter_count = self.operations[opcode].parameter_count()
+        parameter_modes = [int(n) for n in str(instruction // 100)]
+        parameter_modes.reverse()
+        parameter_modes += ([0] *(parameter_count - len(parameter_modes)))
+        return (opcode, parameter_modes)
 
 def run(intcodes):
     proc = IntcodeProcessor()
     proc.Process(intcodes)
     return intcodes
 
-class UnitTests(unittest.TestCase):
+class IntcodeProcessorTests(unittest.TestCase):
     def test_position_mode(self):
         # add
         self.assertEqual(run([1,5,6,3,99,25,35]), [1,5,6,60,99,25,35])
         # mul
         self.assertEqual(run([2, 5, 6, 3, 99, 25, 35]), [2, 5, 6, 875, 99, 25, 35])
+
     def test_immediate_mode(self):
         # add
         self.assertEqual(run([1101, 5, 6, 3, 99, 25, 35]), [1101, 5, 6, 11, 99, 25, 35])
         # mul
-        self.assertEqual(run([1102, 5, 6, 3, 99, 25, 35]), [1, 5, 6, 30, 99, 25, 35])
+        self.assertEqual(run([1102, 5, 6, 3, 99, 25, 35]), [1102, 5, 6, 30, 99, 25, 35])
+
+    def test_split_oppcode(self):
+        proc = IntcodeProcessor()
+        self.assertEqual(proc.split_instruction(1), (1,[0,0,0]))
+        self.assertEqual(proc.split_instruction(101), (1, [1, 0, 0]))
+        self.assertEqual(proc.split_instruction(1101), (1, [1, 1, 0]))
+        self.assertEqual(proc.split_instruction(11101), (1, [1, 1, 1]))
+        self.assertEqual(proc.split_instruction(10101), (1, [1, 0, 1]))
+
+    def test_day2_unittests(self):
+        #regression test to make sure the programs from day 2 still run
+        self.assertEqual(run([1, 9, 10, 3, 2, 3, 11, 0, 99, 30, 40, 50]),
+                         [3500, 9, 10, 70, 2, 3, 11, 0, 99, 30, 40, 50])
+        self.assertEqual(run([1, 0, 0, 0, 99]), [2, 0, 0, 0, 99])
+        self.assertEqual(run([2, 3, 0, 3, 99]), [2, 3, 0, 6, 99])
+        self.assertEqual(run([2, 4, 4, 5, 99, 0]), [2, 4, 4, 5, 99, 9801])
+        self.assertEqual(run([1, 1, 1, 4, 99, 5, 6, 0, 99]), [30, 1, 1, 4, 2, 5, 6, 0, 99])
